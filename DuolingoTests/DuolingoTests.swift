3b637f075c0c8fb83b017bc642b1ab9f36b59265
Van Nguyen <van.le.nguyen1991@gmail.com>
2016-03-04 12:03:18 -0500

    Initial Commit

---
+++ DuolingoTests/DuolingoTests.swift
@@ -0,0 +1,36 @@
+//
+//  DuolingoTests.swift
+//  DuolingoTests
+//
+//  Created by Van Le Nguyen on 3/4/16.
+//  Copyright © 2016 Van Le Nguyen. All rights reserved.
+//
+
+import XCTest
+@testable import Duolingo
+
+class DuolingoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
